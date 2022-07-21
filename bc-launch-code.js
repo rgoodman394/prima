1c1b3d98221bfdc8dcfad1779df75c646b7c0393
Bekah Reese <bekah.reese@primasupply.com>
2022-07-21 14:22:11 -0400

    add shipping-info and launch .js

---
+++ bc-launch-code.js
@@ -0,0 +1,5 @@
+javascript:(function()
+    {location.href = "https://" + window.location.hostname + "/manage/products/edit/" + document.getElementsByName('product_id')[0].value;}
+    )();
+
+    // code to launch BigCommerce product listing pages. Copy and paste into console in dev tools
